40dd226dedc79ea9fe80b695c183ec7418e96e3b
JeevanJyothi729 <munthamallajeevanjyothi95@gmail.com>
2023-08-10 15:55:55 +0530

    hgf

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Companies.css";
+const Companies = () => {
+    return (
+        <section className="c-wrapper">
+            <div className="paddings innerWidth flexCenter c-container">
+               <img src="https://github.com/ZainRk/RealEstate-web-starterpack/blob/master/public/prologis.png?raw=true" alt="" />
+               <img src="https://github.com/ZainRk/RealEstate-web-starterpack/blob/master/public/tower.png?raw=true" alt="" />
+               <img src="https://github.com/ZainRk/RealEstate-web-starterpack/blob/master/public/equinix.png?raw=true" alt="" />
+               <img src="https://github.com/ZainRk/RealEstate-web-starterpack/blob/master/public/realty.png?raw=true" alt="" />
+            </div>
+        </section>
+    );
+};
+
+export default Companies;
